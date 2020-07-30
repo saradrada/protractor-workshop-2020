61d87e25b5ea48ba9b6131fd678b7cbfaba963ab
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-07-30 09:32:43 -0500

    Add report to console

---
+++ protractor/helpers/reporter.ts
@@ -0,0 +1,11 @@
+import { SpecReporter, StacktraceOption } from "jasmine-spec-reporter";
+
+export let reporter = () => {
+  jasmine.getEnv().addReporter(
+    new SpecReporter({
+      spec: {
+        displayStacktrace: StacktraceOption.PRETTY,
+      },
+    })
+  );
+};
