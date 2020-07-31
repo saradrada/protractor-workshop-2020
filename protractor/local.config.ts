8e410566601a79d9a3d9612bd4f7f88c186225e2
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-07-31 08:10:42 -0500

    Deactivated promise handler and Selenium server

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,9 +4,9 @@ import { reporter } from "./helpers/reporter";
 export const config: Config = {
   framework: "jasmine",
   specs: ["../test/google.spec.js"],
-  seleniumAddress: "http://localhost:4444/wd/hub",
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
   },
+  SELENIUM_PROMISE_MANAGER: false,
 };
